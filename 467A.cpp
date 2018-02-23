e385fc8c422ca6b0b6252f2138a7ef7fdb3a1121
thecsw <sagindyk.urazayev@protonmail.ch>
2018-02-23 22:11:02 +0400

    Some formatting

--- 467A.cpp
+++ 467A.cpp
@@ -1,19 +1,19 @@
-//Sagindyk Urazayev
-//http://codeforces.com/problemset/problem/467/A
+// Sagindyk Urazayev
+// http://codeforces.com/problemset/problem/467/A
 #include <iostream>
 
 using namespace std;
 
 int main() {
-    int n, i, room, q, p;
-    room=0;
-    cin >> n;
-    for (i=0; i<n; i++) {
-        cin >> p >> q;
-        if ((q-p)>1) {
-            room++;
-        }
-    }
-    cout << room << endl;
-    return 0;
+	int n, i, room, q, p;
+	room = 0;
+	cin >> n;
+	for (i = 0; i < n; i++) {
+		cin >> p >> q;
+		if ((q - p) > 1) {
+			room++;
+		}
+	}
+	cout << room << endl;
+	return 0;
 }
